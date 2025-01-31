4fc4f9717fbb06c69fd321d7d1c4aa823b5e4e04
Minal Bhardwaj <mahvenu@gmail.com>
2025-01-31 21:47:43 +0000

    customer info page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import CustomerInfoForm from './CustomerInfoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Customer Info App</h1>
+      <CustomerInfoForm />
+    </div>
+  );
+}
+
+export default App;
